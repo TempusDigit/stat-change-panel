39f87914e70edcbdf27523c624a33baf638baca4
TempusDigit <tomas.can.malins@gmail.com>
2024-01-04 10:42:43 +0200

    Refactor.

--- src/components/StatChangePanel.tsx
+++ src/components/StatChangePanel.tsx
@@ -2,23 +2,16 @@ import React, { useMemo } from 'react';
 import { PanelProps } from '@grafana/data';
 import { StatChangeOptions } from 'models.gen';
 import { BigValue, BigValueColorMode, BigValueTextMode, useTheme2 } from '@grafana/ui';
-import { getDisplayValue } from 'utils';
+import { getInfo } from 'utils';
 import { PanelDataErrorView } from '@grafana/runtime';
-import { Info } from 'types';
 
 interface Props extends PanelProps<StatChangeOptions> { }
 
 export const SimplePanel: React.FC<Props> = ({ data, fieldConfig, height, id, options, width }) => {
   const theme = useTheme2();
-  const info: Info = useMemo(() => {
-    try {
-      return { data: getDisplayValue(data.series, options, theme) };
-    } catch (e: any) {
-      return { warning: e.message };
-    }
-  }, [data.series, options, theme]);
+  const info = useMemo(() => getInfo(data.series, options, theme), [data.series, options, theme]);
 
-  if ('warning' in info) {
+  if (info.warning) {
     return <PanelDataErrorView
       panelId={id}
       fieldConfig={fieldConfig}
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import { DataFrame, DisplayValue, FieldType, GrafanaTheme2 } from '@grafana/data';
 import { CURRENT_FIELD, PREVIOUS_FIELD, SERIES, VALUE_INDEX } from 'consts';
 import { StatChangeOptions } from 'models.gen';
+import { Info } from 'types';
 
 function validateSeries(series: DataFrame[]): void {
     let errorMessage = '';
@@ -25,16 +26,7 @@ function validateSeries(series: DataFrame[]): void {
     }
 }
 
-function getValueDifference(series: DataFrame[]): number {
-    const previous = series[SERIES].fields[PREVIOUS_FIELD].values.get(VALUE_INDEX);
-    const current = series[SERIES].fields[CURRENT_FIELD].values.get(VALUE_INDEX);
-
-    return current - previous;
-}
-
-function getColor(series: DataFrame[], options: StatChangeOptions, theme: GrafanaTheme2): string {
-    const valueDifference = getValueDifference(series);
-
+function getColor(valueDifference: number, options: StatChangeOptions, theme: GrafanaTheme2): string {
     let color: string;
     if (valueDifference > 0) {
         color = options.positive;
@@ -47,15 +39,24 @@ function getColor(series: DataFrame[], options: StatChangeOptions, theme: Grafan
     return theme.visualization.getColorByName(color);
 }
 
-export function getDisplayValue(series: DataFrame[], options: StatChangeOptions, theme: GrafanaTheme2): DisplayValue {
+function getDisplayValue(series: DataFrame[], options: StatChangeOptions, theme: GrafanaTheme2): DisplayValue {
     validateSeries(series);
 
+    const previousValue: number = series[SERIES].fields[PREVIOUS_FIELD].values.get(VALUE_INDEX);
     const currentValue: number = series[SERIES].fields[CURRENT_FIELD].values.get(VALUE_INDEX);
 
     return {
         text: currentValue.toString(),
         numeric: currentValue,
-        color: getColor(series, options, theme),
+        color: getColor(currentValue - previousValue, options, theme),
         title: series[SERIES].fields[CURRENT_FIELD].name,
     };
+}
+
+export function getInfo(series: DataFrame[], options: StatChangeOptions, theme: GrafanaTheme2): Info {
+    try {
+        return { data: getDisplayValue(series, options, theme) };
+    } catch (e: any) {
+        return { warning: e.message };
+    }
 }
